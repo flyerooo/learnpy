c17b5e1913e4f143b4f204355e98864ce363acdb
flyerooo <wxlong@foxmail.com>
2016-01-10 16:59:45 +0800

    完成习题7.2

--- exercise7_2.py
+++ exercise7_2.py
@@ -1,11 +1,38 @@
 class Stock:
-    def __init__(self, symbol, name, perviousClosingPrice, currentPrice):
-        self.symbol = symbol
-        self.name = name
-        self.__perviousClosingPrice = perviousClosingPrice
+    # Construct a stock object
+    def __init__(self, name, symbol, previousPrice, currentPrice):
+        self.__name = name
+        self.__symbol = symbol
+        self.__previousPrice = previousPrice
         self.__currentPrice = currentPrice
-    def getName(self):
-        return self.name
-    def getSymbol(self):
-        return self.symbol
 
+    def getName(self):
+        return self.__name
+
+    def getSymbol(self):
+        return self.__symbol
+
+    def getPreviousPrice(self):
+        return self.__previousPrice
+
+    def getCurrentPrice(self):
+        return self.__currentPrice
+
+    def setPreviousPrice(self, previousPrice):
+        self.__previousPrice = previousPrice
+
+    def setCurrentPrice(self, currentPrice):
+        self.__currentPrice = currentPrice
+
+    def getHeight(self):
+        return self.height
+
+    def getChangePercent(self):
+        return format((self.__currentPrice - self.__previousPrice) * 100 / self.__previousPrice, "5.2f") + "%"
+
+def main():
+
+    stock = Stock("Intel", "INTC", 20.5, 20.35)
+    print("The price change is", stock.getChangePercent())
+
+main()
