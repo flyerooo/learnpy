05577f62b08abf8e83b682cab0781f20292ef5b7
flyerooo <wxlong@foxmail.com>
2016-02-27 23:19:27 +0800

    编程练习

--- lianxi.py
+++ lianxi.py
@@ -1,20 +1,13 @@
 #coding=utf-8
-class Word(str):
-    '''存储单词的类，定义比较单词的几种方法'''
+class Person(object):
+    def __init__(self, name):
+        self.name = name
+    def getName(self):
+        return self.name
+    def color(self, color):
+        print "%s is %s" % (self.name,color)
 
-    def __new__(cls, word):
-        # 注意我们必须要用到__new__方法，因为str是不可变类型
-        # 所以我们必须在创建的时候将它初始化
-        if ' ' in word:
-            print "Value contains spaces. Truncating to first space."
-            word = word[:word.index(' ')] #单词是第一个空格之前的所有字符
-        return str.__new__(cls, word)
 
-    def __gt__(self, other):
-        return len(self) > len(other)
-    def __lt__(self, other):
-        return len(self) < len(other)
-    def __ge__(self, other):
-        return len(self) >= len(other)
-    def __le__(self, other):
-        return len(self) <= len(other)
+girl = Person('cangloashi')
+
+name =girl.getName()
